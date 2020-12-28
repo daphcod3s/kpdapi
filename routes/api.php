a85504efb29f7378ad91baa31210c013b68fb228
Onessimus Lyngdoh <auditoree9@gmail.com>
2020-12-28 15:07:59 +0530

    Removed comment from api routes

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\CuisineController;
 
-//Auth Routes
-Route::group(['middleware' => 'api', 'prefix' => 'auth'], function(){
+Route::group(['prefix' => 'auth'], function(){
 	Route::post('login', [AuthController::class, 'login']);
 	Route::post('logout', [AuthController::class, 'logout']);
 	Route::post('refresh', [AuthController::class, 'refresh']);
