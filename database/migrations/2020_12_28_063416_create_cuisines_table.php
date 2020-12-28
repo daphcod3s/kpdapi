f3546a572c4b03e599967aa96038ab73afd43a39
Onessimus Lyngdoh <auditoree9@gmail.com>
2020-12-28 12:09:01 +0530

    Created migration for cuisines table

---
+++ database/migrations/2020_12_28_063416_create_cuisines_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCuisinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cuisines', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->boolean('is_active')->default(false);
+            $table->unsignedBigInteger('created_by');
+            $table->timestamps();
+
+            $table->foreign('created_by')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cuisines');
+    }
+}
